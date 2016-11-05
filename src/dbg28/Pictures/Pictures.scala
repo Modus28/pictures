2168d311fbe805a3a27dde93bf6747c47b5b0489
Daniel <dbg28@case.edu>
2016-11-05 02:20:51 -0400

    Added comments to unwritten methods for handling input

--- src/dbg28/Pictures/Pictures.scala
+++ src/dbg28/Pictures/Pictures.scala
@@ -2,15 +2,16 @@ package dbg28.Pictures
 
 
 /**
-  * Pictures: Accepts text files describing pictures and converts them into relations
+  * Pictures: Accepts Lists of Strings describing pictures and converts them into relations
+  *
   *
   */
-class Pictures(graphs: List[Set[Point]], merged: Set[Point]){
+class Pictures(graphs: List[Graph] = List.empty[Graph], merged: Graph){
 
   var relations: List[IsLayeredOver] = List()
 
 
-  // perform checks to test for invalid graphs
+
   // check the intersect of each set inside merged for which graph is layered over another, and add a relation for it
   // Merge relations into a single ordered string
 
