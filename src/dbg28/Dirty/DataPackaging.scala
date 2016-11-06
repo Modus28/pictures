6b635cc2091597caa702adada3790be19bbabf6a
Daniel <dbg28@case.edu>
2016-11-06 14:15:43 -0500

    Added functionality to split String into Lists of Strings that are each a single graph

--- src/dbg28/Dirty/DataPackaging.scala
+++ src/dbg28/Dirty/DataPackaging.scala
@@ -23,9 +23,16 @@ object DataPackaging {
   def processInput(input: List[String]): Unit = {
     // Parse out the first two lines as integers and set those as locally stored variables
     val dimensions = parseDimensions(input)
-    InputVerification.setDimensions(dimensions.head, dimensions(1))
+    val rows  = dimensions.head
+    val columns = dimensions(1)
+    InputVerification.setDimensions(rows, columns)
 
-    // For each set of text between empty lines, create a Set of Points for it,
+    // The user input without the rows and columns and empty lines
+    val inputWithoutDimensions = input diff List(input.head, input(1),"")
+    val rowInt: Int = rows.toInt + 1
+    val graphStrings: List[List[String]] = inputWithoutDimensions.sliding(rowInt, rowInt-1).toList // check toInt failures
+    println(graphStrings)
+
     // send all but the last one of the list of these sets to a Pictures process method
     // Send the final set, the completed graph, to a separate processing method
   }
@@ -35,10 +42,15 @@ object DataPackaging {
     * @param toCheck the UserInput list to find dimensions in
     * @return the Strings containing the graph dimensions
     */
-  def parseDimensions(toCheck: List[String]): List[String] = {
-    val rows = graphPattern.findFirstMatchIn(toCheck.head)
-    val columns = graphPattern.findFirstMatchIn(toCheck(1))
-    List(rows.get.group(0), columns.get.group(0))
+  private def parseDimensions(toCheck: List[String]): List[String] = {
+    val rows = rowOrColumnPattern.findFirstMatchIn(toCheck.head)
+    val columns = rowOrColumnPattern.findFirstMatchIn(toCheck(1))
+    if(rows.isEmpty | columns.isEmpty){
+      throw new IllegalArgumentException("First two lines do not contain rows and columns")
+    }
+    else{
+      List(rows.get.group(0), columns.get.group(0))
+    }
   }
 }
 
