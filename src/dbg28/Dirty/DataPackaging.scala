e16509a0d8e9cfbdde927429c03947160fdace4f
Daniel <dbg28@case.edu>
2016-11-06 12:40:53 -0500

    Created method in InputHandler to accept input and pass it on to a packaging unit. Created a packaging unit to turn the data into Graphs of Points, and pass them to a verification singleton

---
+++ src/dbg28/Dirty/DataPackaging.scala
@@ -0,0 +1,24 @@
+package dbg28.Dirty
+
+
+/**
+  * EECS 293
+  * Created by Daniel on 11/6/2016.
+  * dbg28@case.edu
+  */
+class DataPackaging {
+
+
+
+  def processInput(input: List[String]): Unit = {
+    // Parse out the first two lines as integers and set those as locally stored variables
+    // For each set of text between empty lines, create a Set of Points for it,
+    // send all but the last one of the list of these sets to a Pictures process method
+    // Send the final set, the completed graph, to a separate processing method
+  }
+}
+
+/** Stores a Set of Points and an upper case character that the points contain */
+case class Graph(points: Set[Point], char: Character)
+/** Point coordinates in a 2D plane */
+case class Point(x: Int, y: Int)
--- src/dbg28/Dirty/InputHandler.scala
+++ src/dbg28/Dirty/InputHandler.scala
@@ -9,95 +9,29 @@ import dbg28.Pictures.Point
   * Created by Daniel on 11/3/2016.
   * dbg28@case.edu
   *
-  * Accepts user input, contains dirty data
+  * Accepts user input and passes it to DataPackaging
+  * On the untrusted side of the barricade
   */
 object InputHandler {
-  val graphPattern = "/([A-Z*])/g".r
-  val rowOrColumnPattern = "(\\d)".r
+  val dataPackaging = new DataPackaging
 
-
-  def processInput(input: List[String]): Unit = {
-    // Parse out the first two lines as integers and set those as locally stored variables
-    // For each set of text between empty lines, create a Set of Points for it,
-    // send all but the last one of the list of these sets to a Pictures process method
-    // Send the final set, the completed graph, to a separate processing method
+  /**
+    *  Method that stores stdIn until it receives two empty lines
+    * @return
+    */
+  private def readInput: Seq[String] = {
+    @tailrec
+    def readRecursively(input: Seq[String], wasEmpty: Boolean): Seq[String] = {
+      val currentLine = scala.io.StdIn.readLine()
+      val currentlyEmpty = currentLine.isEmpty
+      if (currentlyEmpty && wasEmpty) input else readRecursively(currentLine +: input, currentlyEmpty)
+    }
+    readRecursively(Seq[String](), false)
   }
 
 
   def main(args: Array[String]) {
-
-
-
-    def read: Seq[String]= {
-    // WE ARE TRYING TO HAVE IT TERMINATE AFTER TWO EMPTY LINES
-      @tailrec
-      def reread(xs: Seq[String], wasEmpty: Boolean): Seq[String] = {
-        val s = scala.io.StdIn.readLine()
-        val currentlyEmpty = s.isEmpty
-        println(s)
-
-        if (currentlyEmpty && wasEmpty) xs else reread(s +: xs, wasEmpty)
-      }
-
-      reread(Seq[String](), false)
-    }
-    val string = read
-
-    println(s"test: $string")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-    val bi: BufferedReader = new BufferedReader(new InputStreamReader(System.in))
-    val str = "Failed logins from:" + "\n" +
-      "   2.115.68.148 (host148-68-static.115-2-b.business.telecomitalia.it): 7 times" + '\n' +
-      "   43.255.188.161: 2968 times" + '\n' +
-      "   47.251.162.66 (insynq-1.border2.sef203.pnap.net): 521 times" + '\n' +
-      "   58.137.72.110: 7 times" + '\n' + "Misc data for testing" + "Illegal users from: " + '\n' +
-      "   27.254.44.45: 1 time" + "\n" +
-      "   59.175.153.96 (96.153.175.59.broad.wh.hb.dynamic.163data.com.cn): 1 time" + '\n' +
-      "   63.251.162.66 (insynq-1.border2.sef005.pnap.net): 521 times" + '\n' +
-      "   58.137.72.110: 5 times" + '\n' +
-      "   83.251.162.66 (insynq-1.border2.sef043.pnap.net): 521 times" + '\n' +
-      "   27.254.44.45: 1 time" + '\n' +
-      "   13.251.162.66 (insynq-1.border2.sef005.pnap.net): 521 times" + '\n' +
-      "   50.62.42.229 (ip-50-62-42-229.ip.secureserver.net): 7 times" + '\n' +
-      "   50.62.42.232 (ip-50-62-42-229.ip.secureserver.thisisatestforalargenamethatwillbeovereightycharacters.net): 6 times"
-
-    System.setIn(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)))
-
-    var lines: List[String] = List.empty[String]
-    var line: String = ""
-    while ({line = bi.readLine(); line} != null){
-      lines ::: List(line)
-    }
-    println(lines(0))
-
-//      for (String numStr: line.split("\\s"))
-//    sum += Integer.parseInt(numStr);
-
-
-
-
-
-
-*/
-
-    // Accept block of input as List of Strings and send it to processor method in Lists of lines
+    val userInput: Seq[String] = readInput
+    dataPackaging.processInput(userInput.toList)
   }
 }
